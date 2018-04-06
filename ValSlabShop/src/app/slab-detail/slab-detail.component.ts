6e473ea72a0e001fe2526149bca85ec61a51536a
Guest User <Guest@epicodus-100.local>
2018-04-06 15:14:49 -0700

    Add components.

---
+++ ValSlabShop/src/app/slab-detail/slab-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slab-detail',
+  templateUrl: './slab-detail.component.html',
+  styleUrls: ['./slab-detail.component.css']
+})
+export class SlabDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
