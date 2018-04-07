82cb74e028c9f57ecbf1495fee91a7293cabfeab
Guest User <Guest@epicodus-100.local>
2018-04-06 17:32:33 -0700

    ADd styling to filter section. Still need to pipe.

--- ValSlabShop/src/app/marketplace/marketplace.component.ts
+++ ValSlabShop/src/app/marketplace/marketplace.component.ts
@@ -12,6 +12,10 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class MarketplaceComponent implements OnInit {
   slabs: FirebaseListObservable<any[]>;
+  species: string[] = ["Black Walnut", "Walnut Burl", "Maple", "Maple Burl", "Oak", "Sequoia", "Redwood", "Fir", "Myrtlewood", "Madrone", "Juniper", "Speciality Speices"];
+  lengths: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12+"];
+  widths: string[] = ["1", "2", "3", "4+"];
+  depths: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12+"];
   currentRoute: string = this.router.url;
   constructor(private router: Router, private slabService: SlabService) { }
   ngOnInit() {
